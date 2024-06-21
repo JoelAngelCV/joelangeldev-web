102e9e2dfee1f0eaf009b123438beaeaee208254
joelangelcv <prueba@correo.com>
2024-06-20 22:49:27 -0500

    primera prueba

---
+++ components/avatar-portfolio.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import MotionTransition from "./transition-component"
+import Image from "next/image"
+
+const AvatarPortfolio = () => {
+  return (
+    <div>
+        <MotionTransition position="bottom" className="bottom-0 left-0 hidden md:inline-block md:absolute">
+            <Image src="/avatar-works.png" width={300} height={300} className="w-full h-full" alt="Avatar portfolio" />
+        </MotionTransition>
+    </div>
+  )
+}
+
+export default AvatarPortfolio
