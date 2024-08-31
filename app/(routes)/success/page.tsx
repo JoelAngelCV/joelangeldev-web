c79693fb50bb8d9f753b5c7f4e87980c03803b67
joelangelcv <prueba@correo.com>
2024-08-30 20:39:34 -0500

    arreglo de link

--- app/(routes)/success/page.tsx
+++ app/(routes)/success/page.tsx
@@ -9,7 +9,7 @@ const SuccessPage = () => {
         <div className="flex flex-col items-center justify-center h-lvh">
           <CircleImage />
           <h1 className="my-20 text-3xl" >Mensaje enviado con éxito</h1>
-          <Link className="my-10 p-3 rounded-xl text-lg bg-secondary hover:bg-opacity-90" href={"/contact"}>volver a la página de contacto</Link>
+          <Link className="my-10 p-3 rounded-xl text-lg bg-secondary hover:bg-opacity-90" href={"/contacto"}>volver a la página de contacto</Link>
         </div>
     </>
   )
