662f618ba77df1352a2f0157fa28d15324505c15
joelangelcv <prueba@correo.com>
2025-05-17 00:30:43 -0500

    portafolio data

--- components/circle-image.tsx
+++ components/circle-image.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const CircleImage = () => {
   return (
-    <div className="bottom-0 right-0 hidden md:inline-block md:absolute">
+    <div className="bottom-0 right-0 hidden lg:inline-block md:absolute">
       <Image src="/circles.png" width={200} height={200} alt="Circle" className="w-full h-full"/>
     </div>
   );
